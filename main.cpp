2a6528a0ef9f5ea98ea7e3215ab1f4915689333d
isrivastav99 <ishaan2014@gmail.com>
2020-10-19 01:22:59 +0530

    final edits

--- main.cpp
+++ main.cpp
@@ -1,17 +1,16 @@
-#include <iostream>
 #include<bits/stdc++.h>
 using namespace std;
 class RainRate_Output{
 public:
     float value;
     float poor_value;
-    float low_value
-    float medium_value
-    float high_value
+    float low_value;
+    float medium_value;
+    float high_value;
     float extreme_value;
-    Speed_Output()
+    RainRate_Output()
     {
-        this->low_values = 0;
+        this->low_value = 0;
         this->poor_value = 0;
         this->medium_value = 0;
         this->high_value = 0;
@@ -24,7 +23,7 @@ public:
         this->poor_value = p;
         this->high_value = h;
         this->extreme_value = e;
-        
+
         // Weighted mean calculation
         float final = (((5 * p) + (11* l) + (21 * m) + (40 * h) + (65 * e))/(p + l + m + h + e));
         this->value = final;
@@ -36,14 +35,14 @@ class Temperature_Input{
 public:
     float value;
     //All between 0-1
-   /*
-        <= 10   ---- poor       (cold)
-        5-25    ---- low        (warm)
-        20-40   ---- medium     (hot)
-        35>=    ---- high       (very hot)
-   */
+    /*
+         <= 10   ---- poor       (cold)
+         5-25    ---- low        (warm)
+         20-40   ---- medium     (hot)
+         35>=    ---- high       (very hot)
+    */
     float low, poor, medium, high;
-    
+
 
     Temperature_Input()
     {
@@ -91,7 +90,7 @@ public:
         }
         else if (t>=40)
         {
-            this->hot = 1;
+            this->high = 1;
         }
     }
 
@@ -100,15 +99,15 @@ class Pressure_Input{
 public:
     float value;
     //All between 0-1
-   /*
-    SEA LEVEL PRESSURE
-        <= 1002   ---- poor       
-        1000-1006    ---- low        
-        1004-1010   ---- medium     
-        1008>=    ---- high       
-   */
+    /*
+     SEA LEVEL PRESSURE
+         <= 1002   ---- poor
+         1000-1006    ---- low
+         1004-1010   ---- medium
+         1008>=    ---- high
+    */
     float low, poor, medium, high;
-    
+
 
     Pressure_Input()
     {
@@ -118,62 +117,47 @@ public:
         this->medium = 0;
         this->high = 0;
     }
-    void press_fuzzify(float p)
-    {
+    void press_fuzzify(float p) {
         this->value = p;
-        if(p<=1000)
-        {
+        if (p <= 1000) {
             this->poor = 1;
-        }
-        else if(p>1000 && p<1003)
-        {
-            float pre1 = (1003 - p)/3;
+        } else if (p > 1000 && p < 1003) {
+            float pre1 = (1003 - p) / 3;
             this->poor = pre1;
-            float pre2 = (p-1000)/3;
+            float pre2 = (p - 1000) / 3;
             this->low = pre2;
-        }
-        else if (p==1003)
-        {
-            float low = 1;
-        }
-        else if(p>1003 && p<1006)
-        {
-            float pre1 = (1006 - p)/3;
+        } else if (p == 1003) {
+            this->low = 1;
+        } else if (p > 1003 && p < 1006) {
+            float pre1 = (1006 - p) / 3;
             this->low = pre1;
-            float pre2 = (p-1003)/3;
+            float pre2 = (p - 1003) / 3;
             this->medium = pre2;
-        }
-        else if (p==1006)
-        {
-            float medium = 1;
-        }
-        else if(p>1006 && p<1009)
-        {
-            float pre1 = (1009 - p)/3;
+        } else if (p == 1006) {
+            this->medium = 1;
+        } else if (p > 1006 && p < 1009) {
+            float pre1 = (1009 - p) / 3;
             this->medium = pre1;
-            float pre2 = (p-1006)/3;
+            float pre2 = (p - 1006) / 3;
             this->high = pre2;
-        }
-        else if (p>=1009)
-        {
+        } else if (p >= 1009) {
             this->high = 1;
         }
     }
-
 };
 class Humidity_Input{
 public:
     float value;
     //All between 0-1
-   /*
-    Humidity percentage
-        <=    ---- poor       
-        -    ---- low        
-        -   ---- medium     
-        >=    ---- high       
-   */
+    /*
+     Humidity percentage
+         <=    ---- poor
+         -    ---- low
+         -   ---- medium
+         >=    ---- high
+    */
     float low, poor, medium, high;
-    
+
 
     Humidity_Input()
     {
@@ -183,44 +167,30 @@ public:
         this->medium = 0;
         this->high = 0;
     }
-    void hum_fuzzify(float h)
-    {
+    void hum_fuzzify(float h) {
         this->value = h;
-        if(h<=30)
-        {
+        if (h <= 30) {
             this->poor = 1;
-        }
-        else if(h>30 && h<=50)
-        {
-            float pre1 = (50 - h)/20;
+        } else if (h > 30 && h < 50) {
+            float pre1 = (50 - h) / 20;
             this->poor = pre1;
-            float pre2 = (h-30)/20;
+            float pre2 = (h - 30) / 20;
             this->low = pre2;
-        }
-        else if (h==50)
-        {
-            float low = 1;
-        }
-        else if(h>50 && h<70)
-        {
-            float pre1 = (70 - h)/20;
+        } else if (h == 50) {
+            this->low = 1;
+        } else if (h > 50 && h < 70) {
+            float pre1 = (70 - h) / 20;
             this->low = pre1;
-            float pre2 = (h-50)/20;
+            float pre2 = (h - 50) / 20;
             this->medium = pre2;
-        }
-        else if (h==70)
-        {
-            float medium = 1;
-        }
-        else if(h>70 && h<=90)
-        {
-            float pre1 = (90 - h)/20;
+        } else if (h == 70) {
+            this->medium = 1;
+        } else if (h > 70 && h < 90) {
+            float pre1 = (90 - h) / 20;
             this->medium = pre1;
-            float pre2 = (h-70)/20;
+            float pre2 = (h - 70) / 20;
             this->high = pre2;
-        }
-        else if (h>=90)
-        {
+        } else if (h >= 90) {
             this->high = 1;
         }
     }
@@ -231,7 +201,7 @@ public:
 
 void Rule_Evaluate(Temperature_Input t, Pressure_Input p, Humidity_Input h)
 {
-    Rain_Output r;
+    RainRate_Output r;
     float rpoor=0, rmedium=0, rlow=0, rhigh = 0, rextreme = 0;
     //Humidity = poor
     if(h.poor && t.low && p.high){
@@ -252,39 +222,39 @@ void Rule_Evaluate(Temperature_Input t, Pressure_Input p, Humidity_Input h)
     if(h.poor && t.medium && p.low){
         rpoor = min(h.poor, min(t.medium, p.low));
     }
-    
+
     //humidity = low
     if(h.low && t.medium && p.medium){
         rpoor = min(h.low, min(t.medium, p.low));
-    } 
+    }
     if(h.low && t.medium && p.medium){
         rpoor = min(h.low, min(t.medium, p.medium));
-    } 
+    }
     if(h.low && t.medium && p.medium){
         rpoor = min(h.low, min(t.medium, p.high));
-    } 
+    }
     if(h.low && t.low && p.low){
         rpoor = min(h.low, min(t.low, p.low));
-    } 
+    }
     if(h.low && t.low && p.medium){
         rmedium = min(h.low, min(t.low, p.medium));
-    } 
+    }
     if(h.low &&t.low && p.high){
         rlow = min(h.low, min(t.low, p.high));
-    } 
-    
-    
+    }
+
+
     if(h.medium && t.low && p.medium){
         rextreme = min(h.medium, min(t.low, p.medium));
     }
-    
+
     if(h.medium && t.poor && p.high){
         rlow = min(h.medium, min(t.poor, p.high));
     }
-   
+
     if(h.medium && t.low && p.high){
         rhigh = min(h.medium, min(t.low, p.high));
-    } 
+    }
     if(h.high && t.poor && p.medium){
         rlow = min(h.high, min(t.poor, p.medium));
     }
@@ -298,16 +268,20 @@ void Rule_Evaluate(Temperature_Input t, Pressure_Input p, Humidity_Input h)
     cout<<"Defuzzified Rain Rate : "<<r.value<<endl;
 }
 
-int main() {
+int main(int argc, char** argv) {
     Temperature_Input t;
-    t.temp_fuzzify(65);
-    cout<<t.warm_value<<endl;
-    cout<<t.cool_value<<" Temp"<<endl;
-    Cover_Input c;
-    c.cover_fuzzify(25);
-    cout<<c.sunny_value<<endl;
-    cout<<c.cloudy_value<<" Cover"<<endl;
-    Rule_Evaluate(t,c);
+    t.temp_fuzzify(stof(argv[1]));
+    //cout<<t.low<<endl;
+    //cout<<t.medium;
+    //cout<<t.cool_value<<" Temp"<<endl;
+    Humidity_Input h;
+    h.hum_fuzzify(stof(argv[2]));
+    //cout<<h.medium<<endl;
+    //cout<<c.cloudy_value<<" Cover"<<endl;
+    Pressure_Input p;
+    p.press_fuzzify(stof(argv[3]));
+    //cout<<p.medium<<endl;
+    Rule_Evaluate(t,p,h);
 
     return 0;
 }
--- script.py
+++ script.py
@@ -1,15 +1,17 @@
 import time
 import os
 import requests
+import json
 
 URL = "api.openweathermap.org/data/2.5/weather"
+URL = "http://" + URL
 
 # location given here
-location = "delhi"
+id1 = '1261481'
 
 key1 = "62a10c0944a1c19ac7611b8f58d24083"
 # defining a params dict for the parameters to be sent to the API
-PARAMS = {:location, 'appid':key1}
+PARAMS = {'id' : id1 , 'appid' : key1 }
 
 # sending get request and saving the response as response object
 r = requests.get(url = URL,params= PARAMS)
@@ -25,11 +27,23 @@ print(data)
 longitude = data['results'][0]['geometry']['location']['lng']
 formatted_address = data['results'][0]['formatted_address']
 
+
 # printing the output
 print("Latitude:%s\nLongitude:%s\nFormatted Address:%s"
       %(latitude, longitude,formatted_address))
 
 os.system("g++ -o output.txt main.cpp")'''
+
+Temperature = data['main']['temp']
+print(Temperature)
+Humidity = data['main']['humidity']
+print(Humidity)
+Wind = data['main']['pressure']
+print(Wind)
+
+os.system("g++ main.cpp -o o.exe")
+
+Temperature = Temperature - 273
 
 num_seconds = 3
 for countdown in reversed(range(num_seconds + 1)):
@@ -38,3 +52,8 @@ for countdown in reversed(range(num_seconds + 1)):
         time.sleep(1)
     else:
         print('Go!')
+os.system("o.exe " + str(Temperature) + " " + str(Humidity) + " " +  str(Wind) + ">>temp.txt")
+#print("o.exe " + str(Temperature) + " " + str(Humidity) + ">>temp.txt")
+f1 = open("temp.txt", "r")
+text1 = f1.read()
+#print(text1)
